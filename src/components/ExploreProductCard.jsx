72991786d55fa0ece89d329a94fb5888737f055a
mohit <mohitbhusal11@gmail.com>
2025-05-19 22:09:01 +0530

    Complete explore screen

--- src/components/ExploreProductCard.jsx
+++ src/components/ExploreProductCard.jsx
@@ -1,9 +1,9 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const ExploreProductCard = ({title, bgColor}) => {
+const ExploreProductCard = ({title, bgColor, borderColor}) => {
   return (
-    <View style={[styles.mainContainer, {backgroundColor: bgColor}]} >
+    <View style={[styles.mainContainer, {backgroundColor: bgColor, borderColor: borderColor}]} >
         <Image style={styles.imgView} source={require("../assets/dummy_ic.png")} />
       <Text style={styles.titleText} >{title}</Text>
     </View>
@@ -19,7 +19,9 @@ const styles = StyleSheet.create({
         flexDirection: "column",
         justifyContent: "space-between",
         alignItems: "center",
-        borderRadius: 18
+        borderRadius: 18,
+        margin: 5,
+        borderWidth: 1
     },
 
     imgView: {
@@ -34,6 +36,7 @@ const styles = StyleSheet.create({
         marginHorizontal: 25,
         fontSize: 16,
         fontWeight: "600",
-        color: "#181725"
+        color: "#181725",
+        textAlign: "center"
     }
 })
--- src/screens/ExploreScreen.jsx
+++ src/screens/ExploreScreen.jsx
@@ -5,64 +5,80 @@ import ExploreProductCard from '../components/ExploreProductCard';
 
 const categories = [
   {
-    categoryName: "Fresh Fruits & Vegetable",
-    bg_color: "#CDE990", // soft green
+    categoryName: 'Fresh Fruits & Vegetable',
+    bg_color: '#CDE990',
+    borderColor: '#A5BF6A', // darkened green
   },
   {
-    categoryName: "Dairy & Eggs",
-    bg_color: "#FFF3B0", // light yellow
+    categoryName: 'Dairy & Eggs',
+    bg_color: '#FFF3B0',
+    borderColor: '#D6C87D', // darker yellow
   },
   {
-    categoryName: "Beverages",
-    bg_color: "#A0CED9", // pastel blue
+    categoryName: 'Beverages',
+    bg_color: '#A0CED9',
+    borderColor: '#76A4AE', // darker blue
   },
   {
-    categoryName: "Snacks & Branded Foods",
-    bg_color: "#FFD6A5", // warm beige
+    categoryName: 'Snacks & Branded Foods',
+    bg_color: '#FFD6A5',
+    borderColor: '#D7AE7E', // darker beige
   },
   {
-    categoryName: "Bakery & Biscuits",
-    bg_color: "#F8C8DC", // baby pink
+    categoryName: 'Bakery & Biscuits',
+    bg_color: '#F8C8DC',
+    borderColor: '#D09FB4', // darker pink
   },
   {
-    categoryName: "Grains, Pulses & Flours",
-    bg_color: "#F7C59F", // light peach
+    categoryName: 'Grains, Pulses & Flours',
+    bg_color: '#F7C59F',
+    borderColor: '#C79B73', // darker peach
   },
   {
-    categoryName: "Meat & Seafood",
-    bg_color: "#C1DBB3", // muted green
+    categoryName: 'Meat & Seafood',
+    bg_color: '#C1DBB3',
+    borderColor: '#99B28D', // darker muted green
   },
   {
-    categoryName: "Cleaning & Household",
-    bg_color: "#B5EAEA", // light aqua
+    categoryName: 'Cleaning & Household',
+    bg_color: '#B5EAEA',
+    borderColor: '#8DC1C1', // darker aqua
   },
   {
-    categoryName: "Personal Care",
-    bg_color: "#D3C0F9", // soft lavender
+    categoryName: 'Personal Care',
+    bg_color: '#D3C0F9',
+    borderColor: '#A394CC', // darker lavender
   },
   {
-    categoryName: "Baby Care",
-    bg_color: "#FFDAC1", // peach-pink
+    categoryName: 'Baby Care',
+    bg_color: '#FFDAC1',
+    borderColor: '#D7B09A', // darker peach-pink
   },
 ];
 
 const ExploreScreen = () => {
   return (
     <View style={styles.mainContainer}>
-      <Text style={styles.titleText} >Find Products</Text>
-      <View style={styles.searchContainer} >
+      <Text style={styles.titleText}>Find Products</Text>
+      <View style={styles.searchContainer}>
         <SearchCard />
       </View>
-      <View style={styles.exploreProductContainer} >
+      <View style={styles.exploreProductContainer}>
         <FlatList
-        data={categories}
-        numColumns={2}
-        horizontal={false}
-        contentContainerStyle={{paddingHorizontal: 25}}
-        ItemSeparatorComponent={() => <View style={{width: 10, height: 10}} /> }
-        renderItem={({item}) => <ExploreProductCard title={item.categoryName} bgColor={item.bg_color} /> }
+          data={categories}
+          numColumns={2}
+          horizontal={false}
+          contentInsetAdjustmentBehavior="automatic"
+          contentContainerStyle={styles.contenContainerStyleSheet}
+          ItemSeparatorComponent={() => <View style={{height: 5}} />}
+          renderItem={({item}) => (
+            <ExploreProductCard
+              title={item.categoryName}
+              bgColor={item.bg_color}
+              borderColor={item.borderColor}
+            />
+          )}
         />
-        
       </View>
     </View>
   );
@@ -75,21 +91,27 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     flexDirection: 'column',
-    paddingTop: 60
+    paddingTop: 60,
   },
 
   titleText: {
-    alignSelf: "center",
+    alignSelf: 'center',
     fontSize: 20,
-    fontWeight: "700",
-    color: "#181725"
+    fontWeight: '700',
+    color: '#181725',
   },
 
   searchContainer: {
-    marginTop: 20
+    marginTop: 20,
   },
 
   exploreProductContainer: {
-    marginTop: 20,
-  }
+    marginTop: 15,
+    flex: 1
+  },
+
+  contenContainerStyleSheet: {
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
 });
